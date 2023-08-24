567a7dd6bd024ae2fead9e607af7f7353ec3f7d7
Mufil Rahman A <ramanizum@gmail.com>
2023-08-24 20:22:24 +0530

    Make it responsive

--- main.js
+++ main.js
@@ -3,3 +3,9 @@ const iframe = document.querySelector("#iframeTarget");
 function openIframe(url){
     iframe.src=url;
 }
+
+function closing(){
+    const sidebar = document.querySelector(`#navmenu`);
+    console.log(sidebar);
+    sidebar.className="d-none";
+}
